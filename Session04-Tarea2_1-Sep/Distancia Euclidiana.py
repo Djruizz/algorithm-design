2d61ce71134b18fad2a3dae9517344e1e0ed0f40
Djruizz <dario.jruiz31@gmail.com>
2025-09-02 12:44:55 -0600

    Cambio el nombre de la carpeta a session04

--- Session04-Tarea2_1-Sep/Distancia Euclidiana.py
+++ Session04-Tarea2_1-Sep/Distancia Euclidiana.py
@@ -1,3 +1,4 @@
+import math
 x1 = float(input("Ingrese la coordenada X del primer punto (x1): "))
 x2 = float(input("Ingrese la coordenada X del segundo punto (x2): "))
 
@@ -5,8 +6,8 @@ y1 = float(input("Ingrese la coordenada Y del primer punto (y1): "))
 y2 = float(input("Ingrese la coordenada Y del segundo punto (y2): "))
 
 
-d = (((x2-x1)**2) + ((y2-y1)**2) )
+d = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
 if(d<0):
     print("Error")
 else:
-    print("La distancia euclidiana es: ", d**0.5)
+    print("La distancia euclidiana es: ", d)
--- Session04-Tarea2_1-Sep/Distancia Haversine.py
+++ Session04-Tarea2_1-Sep/Distancia Haversine.py
@@ -12,6 +12,7 @@ lon2_rad = math.radians(lon2)
 
 radioTierra = 6371  
 
-d = 2 * radioTierra * math.asin(math.sqrt(math.sin((lat2_rad - lat1_rad) / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin((lon2_rad - lon1_rad) / 2)**2))
+d = 2 * radioTierra * math.asin(math.sqrt(math.sin((lat2_rad - lat1_rad) / 2)**2
+ + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin((lon2_rad - lon1_rad) / 2)**2))
 
 print("La distancia es: ", round(d, 3), " km")
